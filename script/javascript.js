eac8591e3f2a3b018123e503fbe587dd5ad6ba96
Brent Gorwin <brent.gorwin@gmail.com>
2019-10-02 08:46:48 -0500

    Added Jquery to the project to help with animations and linked javascipt folder/file to the project

--- script/javascript.js
+++ script/javascript.js
@@ -2,3 +2,9 @@ window.onload = function fadeInHeaderTop(){
   document.getElementById("top").style.opacity = 1;
   document.getElementById("austinTitle").style.opacity = 1;
 };
+
+
+$("#photo1").scroll(function() {
+  $("#photo1").slideDown("1000");
+  
+});
